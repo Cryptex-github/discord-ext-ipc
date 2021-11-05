7bdd9081ae39585f7acee41cf893c3d1b09c44de
Restyled.io <commits@restyled.io>
2021-11-05 23:57:17 +0000

    Restyled by black

--- discord/ext/ipc/server.py
+++ discord/ext/ipc/server.py
@@ -149,15 +149,13 @@ class Server:
 
             if not headers or headers.get("Authorization") != self.secret_key:
                 log.info(
-                    "Received unauthorized request (Invalid or no token provided).")
-                response = {
-                    "error": "Invalid or no token provided.", "code": 403}
+                    "Received unauthorized request (Invalid or no token provided)."
+                )
+                response = {"error": "Invalid or no token provided.", "code": 403}
             else:
                 if not endpoint or endpoint not in self.endpoints:
-                    log.info(
-                        "Received invalid request (Invalid or no endpoint given).")
-                    response = {
-                        "error": "Invalid or no endpoint given.", "code": 400}
+                    log.info("Received invalid request (Invalid or no endpoint given).")
+                    response = {"error": "Invalid or no endpoint given.", "code": 400}
                 else:
                     server_response = IpcServerResponse(request)
                     try:
@@ -230,8 +228,7 @@ class Server:
             headers = request.get("headers")
 
             if not headers or headers.get("Authorization") != self.secret_key:
-                response = {
-                    "error": "Invalid or no token provided.", "code": 403}
+                response = {"error": "Invalid or no token provided.", "code": 403}
             else:
                 response = {
                     "message": "Connection success",
@@ -260,10 +257,10 @@ class Server:
 
         if self.do_multicast:
             self._multicast_server = aiohttp.web.Application()
-            self._multicast_server.router.add_route(
-                "GET", "/", self.handle_multicast)
+            self._multicast_server.router.add_route("GET", "/", self.handle_multicast)
 
-            self.loop.run_until_complete(self.__start(
-                self._multicast_server, self.multicast_port))
+            self.loop.run_until_complete(
+                self.__start(self._multicast_server, self.multicast_port)
+            )
 
         self.loop.run_until_complete(self.__start(self._server, self.port))
